591e0a753fe6c3681d9ddd9d14d1f273ab3f76b6
dwalsh01 <daraghwalsh97@gmail.com>
2019-01-17 23:27:40 +0000

    added license

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ToolbarHeader from './ToolbarHeader';
+import GridCards from './GridCards';
 
 const App = () => (
   <div>
     <ToolbarHeader>
-      <div>Hello World</div>
+      <GridCards />
     </ToolbarHeader>
   </div>
 );
---
+++ src/components/GridCards.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import FormLabel from '@material-ui/core/FormLabel';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import Radio from '@material-ui/core/Radio';
+import Paper from '@material-ui/core/Paper';
+import SimpleCard from './HomePageCard';
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1
+  },
+  paper: {
+    height: 140,
+    width: 100
+  },
+  control: {
+    padding: theme.spacing.unit * 2
+  }
+});
+
+class GuttersGrid extends React.Component {
+  state = {
+    spacing: '16'
+  };
+
+  handleChange = key => (event, value) => {
+    this.setState({
+      [key]: value
+    });
+  };
+
+  render() {
+    const { classes } = this.props;
+    const { spacing } = this.state;
+    return (
+      <Grid container className={classes.root} spacing={16}>
+        <Grid item xs={12}>
+          <Grid container className={classes.demo} justify="center" spacing={Number(spacing)}>
+            {[0, 1, 2].map(value => (
+              <Grid key={value} item>
+                <SimpleCard />
+              </Grid>
+            ))}
+          </Grid>
+        </Grid>
+      </Grid>
+    );
+  }
+}
+
+export default withStyles(styles)(GuttersGrid);
