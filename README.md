144e6d8e8c2d98474789f8a39f6704e3f44f91cf
dwalsh01 <daraghwalsh97@gmail.com>
2019-01-17 23:08:08 +0000

    initial scaffolding

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 ## Research Web Application
 
 Application built for 3rd Year University group project
+
+## Formatting
+
+Follow this link for formatting:
+
+https://github.com/paulolramos/eslint-prettier-airbnb-react
---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ToolbarHeader from './ToolbarHeader';
+
+const App = () => (
+  <div>
+    <ToolbarHeader>
+      <div>Hello World</div>
+    </ToolbarHeader>
+  </div>
+);
+
+export default App;
