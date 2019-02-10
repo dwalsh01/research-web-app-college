3c96e0bf9a9288f3c681b32163bb2f31eec3b069
ahmadt312 <32881885+ahmadt312@users.noreply.github.com>
2019-02-10 17:07:17 +0000

    LoginForm Upload

--- src/router/routes.jsx
+++ src/router/routes.jsx
@@ -4,11 +4,13 @@ import GridCards from '../components/GridCards';
 import NoMatch from '../components/404';
 import Registration from '../components/Registration';
 import Landing from '../components/Landing';
+import LoginForm from '../components/login';
 
 const Routes = () => (
   <Switch>
     <Route exact path="/" component={Landing} />
     <Route path="/registration" component={Registration} />
+    <Route path="/login" component={LoginForm}/>
     <Route component={NoMatch} />
   </Switch>
 );
